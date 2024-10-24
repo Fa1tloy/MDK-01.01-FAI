a4f26d6312f5f53717b0dfe9cf799de967019180
Andrey-Shelgunov <selgunova3@gmail.com>
2024-10-24 11:30:36 +0300

    LR_2

---
+++ Лабораторная 2/Istoriaprodazh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_2
+{
+    public struct Prodazha
+    {
+        public int kolishestvo;
+        public int data;
+        public Prodykti product;
+    }
+    internal class Istoriaprodazh
+    {
+        Dictionary<tovarniegryppiprodyktov, List<Prodazha>> dannie;
+        public void ADDdanie(int kolishestvo_2, int data_2, Prodykti product_2)
+        {
+            dannie.Add(kolishestvo_2 = 2, data_2 = 6,);
+        }
+    }
+   
+}
